59129b1a72dadd2e33a26d2180c8a4cdd3906999
naveen0-0 <Naveen>
2021-03-07 09:58:59 +0530

    Webpack react starter -- js version

---
+++ README.md
@@ -0,0 +1,1 @@
+Reactjs webpack babel starter pack
---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+   return (
+      <div>
+         My App component
+      </div>
+   )
+}
